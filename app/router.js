f5882703945f1c54cf8f0e1e000f74bff294d423
Jaco Joubert <joubert.jaco@gmail.com>
2016-10-16 21:29:08 -0400

    First commit.

---
+++ app/router.js
@@ -0,0 +1,32 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+const Router = Ember.Router.extend({
+  location: config.locationType
+});
+
+Router.map(function() {
+  this.route('news', function() {
+    this.route('view', { path: ':news_id' });
+  });
+
+  this.route('team', function() {
+    this.route('view', { path: ':team_id' });
+  });
+
+  this.route('approach', function() {
+    this.route('adding-value');
+    this.route('transaction-types');
+    this.route('deal-process');
+  });
+
+  this.route('companies', function() {
+    this.route('view', { path: ':company_id' });
+  });
+
+  this.route('contact');
+
+  this.route('page-not-found', { path: '/*wildcard' });
+});
+
+export default Router;
