d5c9da73d249667a1e248e7d0bdd7156aac61d70
Jaco Joubert <joubert.jaco@gmail.com>
2016-10-19 10:51:57 -0400

    Don't scroll to top when viewing a company.

--- app/pods/companies/view/route.js
+++ app/pods/companies/view/route.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  activate() {
+    // Don't scroll to top.
+  },
+
   model(attrs) {
     return this.modelFor('companies').findBy('id', parseInt(attrs.company_id));
   }
