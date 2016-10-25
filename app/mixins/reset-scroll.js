10752bee4971b9e97906d275227eeafb2483584d
Jaco Joubert <joubert.jaco@gmail.com>
2016-10-25 14:26:58 -0400

    Change how backgrounds work.

--- app/mixins/reset-scroll.js
+++ app/mixins/reset-scroll.js
@@ -3,6 +3,9 @@ import Ember from 'ember';
 export default Ember.Mixin.create({
   activate() {
     this._super();
-    window.scrollTo(0,0);
+    
+    if ($('.app-background')[0]) {
+      $('.app-background')[0].scrollTop = 0;
+    }
   }
 });
